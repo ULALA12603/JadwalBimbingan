c21f66fdcf254dcb98a06b11e7973464e11c1a71
Ulala <145423441+ULALA12603@users.noreply.github.com>
2023-11-01 18:08:18 +0700

    Delete segmentdosen/segmentdosen directory

--- segmentdosen/segmentdosen/fragment/login-dosen.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // Periksa apakah form telah disubmit
-    if (isset($_POST['submit'])) {
-        echo "<script>alert('BERHASIL MASUK'); window.location.href='../dashboard.php';</script>";
-        exit();
-    }
-?>
